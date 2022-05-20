df9e72a425013d66ae03ca6943763001bc1ff64b
Kadrik-31 <87438185+Kadrik-31@users.noreply.github.com>
2022-05-20 12:40:43 +0200

    sd Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 print ("test1")
 add code
 =======
 print ("test2")
+=======
+print ("hello")
+>>>>>>> aa01ae7 (Update test.py)
 add code
 >>>>>>> 3edba8d (Update test.py)
